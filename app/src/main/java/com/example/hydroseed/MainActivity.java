d3f6b5f87fbecd2cbfd6d89e44121cb3e3a58af1
jesusbeltran <jesusbeltran234@gmail.com>
2021-04-01 15:19:47 -0700

    Adding Link access to app using chorme as browser. Links can be changed later. - JB

--- app/src/main/java/com/example/hydroseed/MainActivity.java
+++ app/src/main/java/com/example/hydroseed/MainActivity.java
@@ -13,9 +13,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -66,8 +68,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    //Shows all options for navigation that are in the drawer each will fetch the layout for that fragment
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //Quick Link URL source:
+        String calTransURL = "https://dot.ca.gov/";
+        //String stormWaterContactURL = "https://www.waterboards.ca.gov/water_issues/programs/stormwater/contact.html";
+        String weatherURL = "https://weather.com/weather/today/l/3e10208cb1c3765b5072191becf9220634b5a329cb7f75865facb1e55b24f4b6";
+        //Check navigation item selected from drawer
         switch (item.getItemId()) {
             case R.id.nav_calculate:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -82,15 +90,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         new SettingFragment()).commit();
                 break;
             case R.id.nav_caltrans:
-                Toast.makeText(this, "Open Web Page: Cal Trans", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Open Web Page: Cal Trans", Toast.LENGTH_SHORT).show();
+                openLink(calTransURL);
                 break;
             case R.id.nav_weather:
-                Toast.makeText(this, "Open Web Page: Weather", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "Open Web Page: Weather", Toast.LENGTH_SHORT).show();
+                openLink(weatherURL);
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    //When back is pressed, sidebar closes first then app
     @Override
     public void onBackPressed() {
         //Close Drawer only if back is pressed
@@ -98,6 +109,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             drawer.closeDrawer(GravityCompat.START);
         } else {//Close Activity as usual if pressed and drawer is closed
             super.onBackPressed();
+        }
+    }
+
+    //Open Link given a URL String
+    public void openLink(String URL){
+        Intent openURL = new Intent(Intent.ACTION_VIEW, Uri.parse(URL));
+        openURL.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        openURL.setPackage("com.android.chrome"); //choose google chrome by default
+        try {
+            this.startActivity(openURL);
+        } catch (ActivityNotFoundException ex) {
+            // Chrome not installed so allow user to choose instead
+            openURL.setPackage(null);
+            this.startActivity(openURL);
         }
     }
 }
